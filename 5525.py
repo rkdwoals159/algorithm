8b28c39c19aa9a460f2e637ed2300ccaf2814d07
rkdwoals159 <rkdwoals159@gmail.com>
2022-05-10 23:52:45 +0900

    실버1 문자열

--- 5525.py
+++ 5525.py
@@ -55,21 +55,26 @@
 # -------------------------------------------------------------------------
 import sys
 
-N = int(input())
-M = int(input())
-S = sys.stdin.readline().rstrip()
+# N = int(input())
+# M = int(input())
+# S = sys.stdin.readline().rstrip()
 
-answer, idx, count = 0, 0, 0
+# answer, idx, count = 0, 0, 0
 
-while idx < (M - 1):
-    if S[idx : idx + 3] == 'IOI':
-        idx = idx + 2
-        count = count + 1
-        if count == N:
-            answer = answer + 1
-            count = count - 1
-    else:
-        idx = idx + 1
-        count = 0
+# while idx < (M - 1):
+#     if S[idx : idx + 3] == 'IOI':
+#         idx = idx + 2
+#         count = count + 1
+#         if count == N:
+#             answer = answer + 1
+#             count = count - 1
+#     else:
+#         idx = idx + 1
+#         count = 0
 
-print(answer)
+# print(answer)
+
+
+a,b = [sys.stdin.readline() for i in range(2)]
+print(a)
+print(b)
