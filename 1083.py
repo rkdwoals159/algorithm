82388ad07e1b89afbe2243a7b75759b6041f13b3
kangjaemin <rkdwoals159@gmail.com>
2022-05-18 00:05:26 +0900

    .

--- 1083.py
+++ 1083.py
@@ -1,17 +1,11 @@
-
 N = int(input())
 case = list(map(int,input().split()))
 answer =[]
 S = int(input())
 while S > 0:
-    if S >= N-1:
-        max_Idx = case.index(max(case))
-        answer.append(case.pop(max_Idx))
-        S -= max_Idx
-    else :
-        max_Idx = case.index(max(case[:S+1]))
-        answer.append(case.pop(max_Idx))
-        S -= max_Idx
+    max_Idx = case.index(max(case[:S+1]))
+    answer.append(case.pop(max_Idx))
+    S -= max_Idx
     if not case : break
 answer.extend(case)
 print(*answer)
