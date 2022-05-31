a6ec0f1c3e11ee65bea3fbae4a9ce7f7037dc131
rkdwoals159 <rkdwoals159@gmail.com>
2022-05-31 20:52:23 +0900

    1012 실버2 dfs

--- 1012.py
+++ 1012.py
@@ -1,56 +1,40 @@
-from collections import deque
 import sys
-sys.setrecursionlimit(10**6)
+sys.setrecursionlimit(10**6) #recursion에러 방지
+
+
+def dfs(cnt,x,y) :
+    global area
+    dir_ = [(0,-1),(0,1),(1,0),(-1,0)]
+    area[y][x] = cnt
+    for i in dir_:
+        dy = y + i[0]
+        dx = x + i[1]
+
+        if 0<=dx<M and 0<=dy<N:
+            if area[dy][dx] == 1:
+                area[dy][dx] = cnt
+                dfs(cnt,dx,dy)
+
 
 T = int(input())
-T_C = deque([])
+T_C = []
 for _ in range(T):
     T_C.append(list(map(int,input().split())))
-    K = T_C[-1][2]
-    case = []
-    for _ in range(K):
-        case.append(list(map(int,input().split())))
-    T_C.append(case)
-
-
+    for __ in range(T_C[-1][2]):
+        T_C.append(list(map(int,input().split())))
 for _ in range(T):
-    M,N,K = T_C.popleft()
-    case = T_C.popleft()
-    area = [[0]*M for _ in range(N)]
-    for a,b in case: area[b][a] = 1
-    dir_ = [(-1,0),(0,1),(1,0),(0,-1)]
-
-    max_num = 2
-    def dfs(x,y):
-        global max_num
-        for i in dir_:
-            dx = x + i[0]
-            dy = y + i[1]
-
-            if 0<=dx <M and 0<=dy<N:
-                if area[dy][dx] ==1:
-                    area[dy][dx] = max_num
-                    dfs(dx,dy)
-    for x,y in case:
-        if area[y][x] != 0: 
-            dfs(x,y)
-            max_num +=1
-            
-
-    # for i in range(N):
-    #     for j in range(M):
-    #         print(area[i][j],end=' ')
-    #     print()
-    # print()
-
-    area = [y for x in area for y in x]
-    # print(area)
-    # print(max(area))
-    answer = area.count(1)
-    area = list(set(area))
-    answer += len(area)-1
-    if 1 in area : answer -=1
-    print(answer)
-
+    M,N,K = T_C.pop(0)
+    area = [[0]*M for __ in range(N)]
+    for __ in range(K):
+        x,y = T_C.pop(0)
+        area[y][x] = 1
+    cnt = 2
+    for y in range(N):
+        for x in range(M):
+            if area[y][x] == 1:
+                dfs(cnt,x,y)
+                cnt +=1
+    area = [element for array in area for element in array]
+    print(max(area)-1)
 
 
